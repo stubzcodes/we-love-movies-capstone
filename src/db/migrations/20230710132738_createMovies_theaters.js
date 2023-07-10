0b30fbe8314db1b09ed0736c5ba709addb5363a2
Yesenia <yesi.garcia0519@gmail.com>
2023-07-10 13:43:46 -0400

    updated createMovies migration

--- src/db/migrations/20230710132738_createMovies_theaters.js
+++ src/db/migrations/20230710132738_createMovies_theaters.js
@@ -1,8 +1,19 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("movies_theaters", (table) => {
+    table
+      .foreign("movie_id")
+      .references("movie_id")
+      .inTable("movies")
+      .onDelete("cascade");
+    table
+      .foreign(`theater_id`)
+      .references(`theater_id`)
+      .inTable(`theaters`)
+      .onDelete(`cascade`);
+    table.boolean(`is_showing`).defaultTo(false);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("suppliers");
 };
